385715378bc38a7968b9332578bb1107610868d7
daviardev <jerson.silva99@hotmail.com>
2023-04-03 23:56:12 -0500

    feat: Add ignore collision with the shell and the player

--- Assets/Scripts/Manager/LevelManager.cs
+++ Assets/Scripts/Manager/LevelManager.cs
@@ -1,18 +1,25 @@
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
-public class LevelManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class LevelManager : MonoBehaviour {
+    #region Layers
+    private Dictionary<string, int> layerMap = new Dictionary<string, int>();
+
+    private void Awake() {
+        // map name the layers to yours index
+        layerMap.Add("Shell", LayerMask.NameToLayer("Shell"));
+        layerMap.Add("Ground", LayerMask.NameToLayer("Ground"));
+        layerMap.Add("Player", LayerMask.NameToLayer("Player"));
+        layerMap.Add("GrabShell", LayerMask.NameToLayer("GrabShell"));
     }
+    #endregion
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Start() {
+        #region Ignore collisions
+        Physics2D.IgnoreLayerCollision(layerMap["Player"], layerMap["Shell"], true);
+        Physics2D.IgnoreLayerCollision(layerMap["GrabShell"], layerMap["Shell"], true);
+        Physics2D.IgnoreLayerCollision(layerMap["GrabShell"], layerMap["Ground"], true);
+        #endregion
     }
 }
